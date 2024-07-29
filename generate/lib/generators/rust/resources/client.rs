51833cb1849e70c325733f874a2453db32220fba
doom <clement.doumergue@epitech.eu>
2024-07-29 19:46:52 +0200

    Fix {Async,}HttpClient traits definitions in the Rust client

--- generate/lib/generators/rust/resources/client.rs
+++ generate/lib/generators/rust/resources/client.rs
@@ -3,22 +3,22 @@ use serde::{de::DeserializeOwned, Serialize};
 pub trait HttpClient {
     type Error;
 
-    fn get<Q, R>(&self, path: &str, query: &Q) -> Result<R, Self::Error>
+    fn get<Q, R>(&self, path: &str, query: Q) -> Result<R, Self::Error>
     where
         Q: Serialize,
         R: DeserializeOwned;
 
-    fn post<B, R>(&self, path: &str, body: &B) -> Result<R, Self::Error>
+    fn post<B, R>(&self, path: &str, body: B) -> Result<R, Self::Error>
     where
         B: Serialize,
         R: DeserializeOwned;
 
-    fn put<B, R>(&self, path: &str, body: &B) -> Result<R, Self::Error>
+    fn put<B, R>(&self, path: &str, body: B) -> Result<R, Self::Error>
     where
         B: Serialize,
         R: DeserializeOwned;
 
-    fn delete<B, R>(&self, path: &str, body: &B) -> Result<R, Self::Error>
+    fn delete<B, R>(&self, path: &str, body: B) -> Result<R, Self::Error>
     where
         B: Serialize,
         R: DeserializeOwned;
@@ -30,36 +30,36 @@ pub trait AsyncHttpClient {
     fn get<Q, R>(
         &self,
         path: &str,
-        query: &Q,
+        query: Q,
     ) -> impl std::future::Future<Output = Result<R, Self::Error>> + Send
     where
-        Q: Serialize,
-        R: DeserializeOwned;
+        Q: Serialize + Send,
+        R: DeserializeOwned + Send;
 
     fn post<B, R>(
         &self,
         path: &str,
-        body: &B,
+        body: B,
     ) -> impl std::future::Future<Output = Result<R, Self::Error>> + Send
     where
-        B: Serialize,
-        R: DeserializeOwned;
+        B: Serialize + Send,
+        R: DeserializeOwned + Send;
 
     fn put<B, R>(
         &self,
         path: &str,
-        body: &B,
+        body: B,
     ) -> impl std::future::Future<Output = Result<R, Self::Error>> + Send
     where
-        B: Serialize,
-        R: DeserializeOwned;
+        B: Serialize + Send,
+        R: DeserializeOwned + Send;
 
     fn delete<B, R>(
         &self,
         path: &str,
-        body: &B,
+        body: B,
     ) -> impl std::future::Future<Output = Result<R, Self::Error>> + Send
     where
-        B: Serialize,
-        R: DeserializeOwned;
+        B: Serialize + Send,
+        R: DeserializeOwned + Send;
 }
